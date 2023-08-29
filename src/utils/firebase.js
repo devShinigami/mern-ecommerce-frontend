5851ba9990d443e348eb945b7c8cce64b52ae67a
Hashir <137887293+devShinigami@users.noreply.github.com>
2023-08-29 14:42:07 +0500

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCjBjQSUplUd-HWakYSqW8wPAXnvTYYaS0",
-  authDomain: "mern-ecommerce-clone.firebaseapp.com",
-  projectId: "mern-ecommerce-clone",
-  storageBucket: "mern-ecommerce-clone.appspot.com",
-  messagingSenderId: "222644639529",
-  appId: "1:222644639529:web:b561318a0c34ceea280938",
-  measurementId: "G-B8TJ14E86V",
-};
+const firebaseConfig = {}
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const firebaseAuth = getAuth(app);
